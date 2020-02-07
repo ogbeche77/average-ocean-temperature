effb747c15860cccbd4b55a29b2d87d366d2b4b5
ogbeche77 <ogbeche77@yahoo.com>
2020-02-07 12:51:10 +0100

    chart dimension

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-var ctx = document.getElementById('chart').getContext('2d');
-var myChart = new Chart(ctx, {
+ // data retrieved on 06.02.2020 from https://data.giss.nasa.gov/gistemp/
+// working with API locally (file saved on localhost)
+
+async function getData() { //named async bcos its an asynchronous function making asynchronous calls
+    const response = await fetch("nasaData.csv");
+    const data = await response.text();  //we decide to receive data as text. it could also be json,blob,arraybuffer,redirect etc
+    console.log(data);
+   
+   //parse the csv; meaning breaking up the data and arranging it objects etc
+    //we use the split function here to put the element in an array
+   
+    //we can split the table with line break since each row is demacated by a "\n"
+    //table could also be demacated with \, \. etc
+    const table = data.split("\n");
+    //row refers to the rows in the table and 
+    //first 3 rows are splited in column
+    table.forEach(row =>{
+        const columns =row.split(",");
+        const year =columns[0];
+        xlabels.push(year); //chart ref can be added here
+        const tempA =columns[1];
+         avgMonths.push(tempA); //chart ref can be added here
+         const tempN =columns[2];
+         avgMonths1.push(tempA); //chart ref can be added here
+        console.log(year,tempA, tempN);
+    });
+}
+
+
+// Presenting the result of the ocean mean temperature in a chart
+const xlabels =[];  //important its made a global variable
+const avgMonths = [];  //important its made a global variable
+const avgMonths1 =[];//important its made a global variable
+
+chartIt();  //calling function (position matter)
+
+async function chartIt(){
+    await getData();  //after chartIt is called, it waits for getData() before executing
+const ctx = document.getElementById('chart').getContext('2d');
+// To present as chart we create a new variable and set to blank array
+const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: xlabels,  //const newLabel is referenced here
         datasets: [{
-            label: 'Ocean mean temperature',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            label: 'Ocean Avg temperature (Northern Hemisphere)',
+            data: avgMonths, avgMonths1,
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)'],
             borderWidth: 1
         }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-        
-    }
-    
-    
-
-});
-
-
-
+    });
+}
+ 
